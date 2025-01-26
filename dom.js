74887cac542f7dc4f4d0538f2a2998cad7642270
Nithish <nithish17012003@gmail.com>
2025-01-26 08:22:13 +0530

    project

--- dom.js
+++ dom.js
@@ -1,12 +1,14 @@
 // console.log(window);
 // console.log(document);
 
+
+
 // let element = document.getElementById("demo");
 // element.innerText = "G+"
 // console.log(element);
 
 // let ele = document.getElementById("test");
-// // ele.innerText= "<h1>Header</h1>"
+ // ele.innerText= "<h1>Header</h1>"
 // ele.innerHTML = "<h1>Header</h1>"
 // console.log(ele);
 
@@ -16,18 +18,17 @@
 // console.log(ele);
 // console.log(Array.isArray(ele));
 // ele[0].innerText="Hello"
-// // ele[0].style.backgroundcolor="tamato"
+// ele[0].style.backgroundcolor="tamato"
 
 
 
-// // !Spread operator :- it will takeout each and every value from the original array 
-// // ! and store it inside one more new array(pure array)
-// //? syntax :- [...variable ];
+ // !Spread operator :- it will takeout each and every value from the original array 
+ // ! and store it inside one more new array(pure array)
+ //? syntax :- [...variable ];
 
-// let x = [...ele];
-// // console.log(x, Array.isArray(x));
+// let x = [...ele]; // console.log(x, Array.isArray(x));
 // x.map(element=>{
-//     // console.log(element)
+    // console.log(element)
     // element.style.backgroundColor="orange"
 // })
 
@@ -56,10 +57,29 @@
 // let ele = document.querySelectorAll(".test");
 // console.log(ele);
 
-let btn = document.querySelector("#btn");
-// ! addEventListner("event", ()=>{})
-    btn.addEventListener("click",()=>{
-        console.log("Button clicked");
-        alert("hi");
-        document.body.style.backgroundColor="tomato"
-    })
+// let btn = document.querySelector("#btn");
+ // ! addEventListner("event", ()=>{})
+//     btn.addEventListener("click",()=>{
+//         console.log("Button clicked");
+//         // alert("hi");
+//         document.body.style.backgroundColor="tomato"
+//     })
+// let divBlock=document.getElementById("divblock");
+// divBlock
+
+
+let ele = document.createElement("h1");
+ele.innerText = "Dynamic creation of html element";
+ele.setAttribute("id", "demo");
+ele.id = "demo";
+ele.removeAttribute("id");
+ console.log(ele);
+
+let image = document.createElement("img");
+image.src = "C:\Users\tamat\OneDrive\Desktop\Training\Training\Screenshot (10).png";
+console.log(image);
+document.body.appendChild(ele);
+document.body.appendChild(image);
+
+
+
---
+++ newFile.js
@@ -0,0 +1,3 @@
+let ele= document.createElement("h1");
+ele.innerText("my name is nithish");
+document.body.appendChild(ele);
