45307d975438af941da9fa115c2fd41121ee91c3
Nithish <nithish17012003@gmail.com>
2025-01-27 18:23:59 +0530

    project

--- dom.js
+++ dom.js
@@ -68,18 +68,89 @@
 // divBlock
 
 
-let ele = document.createElement("h1");
-ele.innerText = "Dynamic creation of html element";
-ele.setAttribute("id", "demo");
-ele.id = "demo";
-ele.removeAttribute("id");
- console.log(ele);
+// let ele = document.createElement("h1");
+// ele.innerText = "Dynamic creation of html element";
+// ele.setAttribute("id", "demo");
+// ele.id = "demo";
+// ele.removeAttribute("id");
+//  console.log(ele);
 
-let image = document.createElement("img");
-image.src = "C:\Users\tamat\OneDrive\Desktop\Training\Training\Screenshot (10).png";
-console.log(image);
-document.body.appendChild(ele);
-document.body.appendChild(image);
+// let image = document.createElement("img");
+// image.src = "C:\Users\tamat\OneDrive\Desktop\Training\Training\Screenshot (10).png";
+// console.log(image);
+// document.body.appendChild(ele);
+// document.body.appendChild(image);
+
+
+// let vid = document.createElement("video");
+// vid.setAttribute("src","./video.mp4");
+// vid.setAttribute("controls","controls")
+// console.log(vid);
+// document.body.appendChild(vid);
 
 
 
+//!
+// let mainEle = document.createElement("div");
+// mainEle.setAttribute("id", "mainBlock");
+// mainEle.style.border="2px solid red";
+// mainEle.style.width="550px";
+// mainEle.style.height="550px";
+// console.log(mainEle);
+
+// let topEle = document.createElement("div");
+// topEle.setAttribute("class", "topBlock");
+// // console.log(topEle);
+
+// let image = document.createElement("img");
+// image.src="https://cdn.britannica.com/70/234870-050-D4D024BB/Orange-colored-cat-yawns-displaying-teeth.jpg "
+// image.style.width = "550px";
+// console.log(image);
+
+
+// let bottomEle = document.createElement("div");
+// bottomEle.setAttribute("class", "bottomBlock");
+// // console.log(bottomEle);
+
+
+
+
+// let h1 = document.createElement("h1");
+// h1.innerText = "CAT";
+// console.log(h1);
+
+// let btn = document.createElement("button");
+// btn.innerText="View More";
+// btn.style.border="none";
+// btn.style.padding="20px";
+// btn.style.backgroundColor="dargerblue";
+// btn.style.color="color";
+
+// bottomEle.appendChild(h1);
+// bottomEle.appendChild(btn);
+
+// topEle.appendChild(image);
+// mainEle.appendChild(topEle);
+// mainEle.appendChild(bottomEle);
+// document.body.appendChild(mainEle);
+
+
+
+// ! 
+
+let form = document.querySelector("form");
+
+let username = document.getElementById("uName");
+let password = document.getElementById("uPass");
+
+let gender = document.getElementsByName("gender");
+console.log(gender);    
+
+form.addEventListener("submit" , event=>{
+    event.preventDefault();
+    let un = username.value;
+    let up = password.value;
+    let gen = gender.value;
+
+    console.log(un , up , gen);
+})
